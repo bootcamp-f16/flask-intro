fab5b17376843ab38a3c8b62924c542e45e40124
Andrew House <andrewhouse621@gmail.com>
2016-08-17 12:03:46 -0400

    Change the Order of Routes

--- hello.py
+++ hello.py
@@ -5,24 +5,9 @@ from wtforms import Form, TextField, TextAreaField, validators, StringField, Int
 
 app = Flask(__name__, template_folder='templates')
 
-class TimesForm(Form):
-    x = IntegerField('x:', validators=[validators.required()])
-    y = IntegerField('y:', validators=[validators.required()])
-
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
-@app.route('/table', methods=['GET', 'POST'] )
-def times():
-    form = TimesForm(request.form)
-    if request.method == 'POST':
-        min = request.form['x'] or 1 
-        max = request.form['y'] or 10 
-    else:
-        min = 1
-        max = 10
-    return render_template('table.html', min=int(min), max=int(max), form=form)
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -33,3 +18,19 @@ def show_user_profile(username):
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return render_template('post.html', post_id=post_id)
+
+class TimesForm(Form):
+    x = IntegerField('x:', validators=[validators.required()])
+    y = IntegerField('y:', validators=[validators.required()])
+
+@app.route('/table', methods=['GET', 'POST'] )
+def times():
+    form = TimesForm(request.form)
+    if request.method == 'POST':
+        min = int(request.form['x'] or 1 )
+        max = int(request.form['y'] or 10 )
+    else:
+        min = 1
+        max = 10
+    return render_template('table.html', min=min, max=max, form=form)
+
